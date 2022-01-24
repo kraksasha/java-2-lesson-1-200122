b670af0e9f1de0646ce31b202c4becfc2574bb0b
kraksasha <kraksasha@yandex.ru>
2022-01-24 17:16:08 +0300

    change

--- src/main/java/com/geekbrains/person/customer/Customer.java
+++ src/main/java/com/geekbrains/person/customer/Customer.java
@@ -99,4 +99,5 @@ public class Customer extends Person {
         this.purchaseList = purchaseList;
     }
 
+
 }
--- src/main/java/com/geekbrains/product/Product.java
+++ src/main/java/com/geekbrains/product/Product.java
@@ -28,4 +28,5 @@ public class Product {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+
 }
